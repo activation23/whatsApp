fee05a9cf39a9793d0d71ce8c706dec539dfdb0a
activation23 <activationauthentiquewindows@gmail.com>
2024-09-15 10:23:16 +0000

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,6 @@ const { Client, LocalAuth } = require('whatsapp-web.js');
 const fs = require('fs');
 const express = require('express');
 const path = require('path');
-const fetch = require('node-fetch'); // Pour envoyer les requêtes HTTP à Telegram
-
-// Variables pour Telegram
-const TELEGRAM_BOT_TOKEN = '7228703186:AAGWIxpeDP9vRgD8vjMY4octzwq9nDIDXkw'; // Remplace par le token de ton bot Telegram
-const TELEGRAM_CHAT_ID = '6143012351'; // Remplace par ton ID de chat Telegram
 
 const client = new Client({
   authStrategy: new LocalAuth(),
@@ -21,43 +16,16 @@ const client = new Client({
 const app = express();
 const port = 3000;
 
+// Serve static files from the 'public' directory
 app.use(express.static('public'));
-
-// Fonction pour envoyer le QR code à Telegram
-async function sendQRToTelegram(filePath) {
-  const formData = new FormData();
-  formData.append('chat_id', TELEGRAM_CHAT_ID);
-  formData.append('photo', fs.createReadStream(filePath));
-
-  try {
-    const response = await fetch(`https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendPhoto`, {
-      method: 'POST',
-      body: formData
-    });
-
-    const result = await response.json();
-    if (result.ok) {
-      console.log('QR code envoyé avec succès à Telegram');
-    } else {
-      console.error('Erreur lors de l\'envoi à Telegram:', result);
-    }
-  } catch (error) {
-    console.error('Erreur lors de l\'envoi à Telegram:', error);
-  }
-}
 
 client.on('qr', qr => {
   // Générer et enregistrer le QR code en format JPEG
   const filePath = `public/qrcode_${Date.now()}.jpeg`; // Le nom du fichier inclut un timestamp unique
 
-  qrcode.toFile(filePath, qr, { type: 'jpeg' }, async (err) => {
-    if (err) {
-      console.error('Erreur lors de la génération du fichier JPEG du QR code:', err);
-    } else {
-      console.log(`QR code généré et enregistré sous ${filePath}`);
-      // Envoyer le QR code à Telegram
-      await sendQRToTelegram(filePath);
-    }
+  qrcode.toFile(filePath, qr, { type: 'jpeg' }, (err) => {
+    if (err) console.error('Erreur lors de la génération du fichier JPEG du QR code:', err);
+    else console.log(`QR code généré et enregistré sous ${filePath}`);
   });
 });
 
@@ -75,6 +43,7 @@ client.on('message', message => {
 
 client.initialize();
 
+// Serve the page with QR code link
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
